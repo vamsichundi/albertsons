70b8c844ccd666eb5deea6d8eaf99e91dc68e66e
Vamsikrishna Chundi <chundi.vamsikrishna@gmail.com>
2024-02-11 20:13:50 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './components/Auth/Login';
+import Layout from './components/dashboard/Layout';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Login /> */}
+     <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/dashboard/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopNav from './TopNav'
+import SideNav from './SideNav'
+
+const Layout = () => {
+  return (
+    <div>
+        <TopNav />
+        <SideNav />
+    </div>
+  )
+}
+
+export default Layout
